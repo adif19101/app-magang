d8292db1d7b574b1424c32e71e2340a8782053f9
Adif <fikriadif7@gmail.com>
2023-06-19 12:26:48 +0700

    fix responsive table button surat

--- app/Views/mahasiswa/surat_mhs.php
+++ app/Views/mahasiswa/surat_mhs.php
@@ -9,27 +9,23 @@
                 <h3 class="card-title col-auto">Surat Magang</h3>
 
                 <div class="col-auto">
-                    <div class="btn-list">
-                        <a href="<?= base_url('mahasiswa/surat/new') ?>" class="btn d-none d-md-inline-flex">
+                    <div class="dropdown">
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M12 5l0 14" />
                                 <path d="M5 12l14 0" />
                             </svg>
                             Ajukan Surat
-                        </a>
-                        <a href="<?= base_url('mahasiswa/surat') ?>" class="btn btn-primary">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-list" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <path d="M9 6l11 0"></path>
-                                <path d="M9 12l11 0"></path>
-                                <path d="M9 18l11 0"></path>
-                                <path d="M5 6l0 .01"></path>
-                                <path d="M5 12l0 .01"></path>
-                                <path d="M5 18l0 .01"></path>
-                            </svg>
-                            Semua
-                        </a>
+                        </button>
+                        <div class="dropdown-menu">
+                            <a href="<?= base_url('mahasiswa/surat/new') ?>" class="dropdown-item">
+                                Permohonan Magang
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                Plot Pembimbing
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -78,6 +74,8 @@
                 targets: [1],
                 orderable: false,
             }, ],
+            scrollX: true,
+            bAutoWidth: false,
         });
 
         var debounce = new $.fn.dataTable.Debounce(datatable);
