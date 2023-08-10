9233262c58f25c44c854a05eb76e26eaf86ae8ab
Adif <fikriadif7@gmail.com>
2023-08-10 21:38:05 +0700

    ganti db

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'app_magang_dev',
+        'database' => 'app_magang',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
